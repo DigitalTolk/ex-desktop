614cf89e1147ab4d30aeae4e68e1e8045579b949
Günter Grodotzki <gunter@grodotzki.com>
2026-05-04 09:53:08 +0200

    Enable Desktop Notifications

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -124,6 +124,7 @@ pub fn run() {
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_opener::Builder::new().build())
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
             if cfg!(debug_assertions) {
